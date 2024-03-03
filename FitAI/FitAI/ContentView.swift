0e2e6ec5a2cabb0f89d400555217c1fc484d93f6
1dillon <dyoungshi004@gmail.com>
2024-03-02 23:15:45 -0500

    Abstracted the Sign In button

--- FitAI/FitAI/ContentView.swift
+++ FitAI/FitAI/ContentView.swift
@@ -11,6 +11,36 @@ import SwiftData
 
 struct ContentView: View {
 
+    // TODO: Create environment object to hold this data
+    @AppStorage("userId") var userId: String = ""
+    @AppStorage("email") var email: String = ""
+    @AppStorage("firstName") var firstName: String = ""
+    @AppStorage("lastName") var lastName: String = ""
+    
+    private var isSignedIn: Bool {
+        !userId.isEmpty
+    }
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                
+                if isSignedIn {
+                    SignInButtonView()
+                }
+                else {
+                    // Signed In
+                    Text("Welcome back \(firstName)!")
+                }
+                
+            }
+            .navigationTitle("Sign In")
+        }
+    }
+}
+
+struct SignInButtonView: View {
+    
     @Environment(\.colorScheme) var colorScheme
     
     // TODO: Create environment object to hold this data
@@ -20,57 +50,44 @@ struct ContentView: View {
     @AppStorage("lastName") var lastName: String = ""
     
     var body: some View {
-        NavigationView {
-            VStack {
+        SignInWithAppleButton(.signIn) { request in
+            
+            request.requestedScopes = [.email, .fullName]
+            
+        } onCompletion: { result in
+            
+            switch result {
+            case .success(let auth):
                 
-                if userId.isEmpty {
-                    SignInWithAppleButton(.signIn) { request in
-                        
-                        request.requestedScopes = [.email, .fullName]
-                        
-                    } onCompletion: { result in
-                        
-                        switch result {
-                        case .success(let auth):
-                            
-                            switch auth.credential {
-                            case let credential as ASAuthorizationAppleIDCredential:
-                                
-                                // User Id
-                                let userId = credential.user
-                                
-                                // User Info
-                                let email = credential.email
-                                let firstName = credential.fullName?.givenName
-                                let lastName = credential.fullName?.familyName
-                                
-                                self.userId = userId
-                                self.email = email ?? ""
-                                self.firstName = firstName ?? ""
-                                self.lastName = lastName ?? ""
-                                
-                            default:
-                                break
-                            }
-                            
-                        case .failure(let error):
-                            print(error)
-                        }
-                        
-                    }
-                    .signInWithAppleButtonStyle(colorScheme == .dark ? .white : .black)
-                    .frame(height: 50)
-                    .padding()
-                    .cornerRadius(8)
+                switch auth.credential {
+                case let credential as ASAuthorizationAppleIDCredential:
+                    
+                    // User Id
+                    let userId = credential.user
+                    
+                    // User Info
+                    let email = credential.email
+                    let firstName = credential.fullName?.givenName
+                    let lastName = credential.fullName?.familyName
+                    
+                    self.userId = userId
+                    self.email = email ?? ""
+                    self.firstName = firstName ?? ""
+                    self.lastName = lastName ?? ""
+                    
+                default:
+                    break
                 }
                 
-                else {
-                    // Signed In
-                }
-                
+            case .failure(let error):
+                print(error)
             }
-            .navigationTitle("Sign In")
+            
         }
+        .signInWithAppleButtonStyle(colorScheme == .dark ? .white : .black)
+        .frame(height: 50)
+        .padding()
+        .cornerRadius(8)
     }
 }
 
